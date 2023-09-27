92bf044586ec09db112ca14206b95ac8dd7be0e6
Shashank Aditya <msrsaditya@gmail.com>
2023-09-27 10:21:09 +0000

    Update essid.sh

--- scripts/Network/essid.sh
+++ scripts/Network/essid.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-# Check if Ethernet is connected
+# Check if Ethernet is Connected, If Yes Show It
 if [[ $(cat /sys/class/net/enp0s20f0u3/carrier) == 1 ]]; then
     ip_addr=$(ip -4 addr show enp0s20f0u3 | grep -oP '(?<=inet\s)\d+(\.\d+){3}' | awk 'NR==1')
     echo "$ip_addr "
-# If Ethernet is not connected, check Wi-Fi
+# If Ethernet is Not Connected, Connect to Wi-Fi and Show it
 else
     essid=$(iwconfig wlan0 | awk -F'"' '/ESSID:/{print $2}' | awk '{print $1}')
     if [ -z "$essid" ]; then
@@ -14,8 +14,7 @@ else
     fi
 fi
 
-# Handle left-click event
+# Left Click (Touch) -- Show List of Available Wi-Fi Networks
 if [ "$BLOCK_BUTTON" -eq 1 ]; then
-    # Left-click - Execute the specified command
     sh /usr/local/bin/Scripts/Network/wifi-connect.sh
 fi
