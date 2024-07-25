dc4a409476261cee1a54702d951a72be5465860f
Shashank Aditya <msrsaditya@gmail.com>
2024-07-25 20:59:33 +0530

    Changes made

--- scripts/Bluetooth/blueicon.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-if bluetoothctl show | grep -q "Powered: yes"; then # If Bluetooth is turned on,
-  echo ""  # Show Bluetooth Icon.
-fi
-
-if [ "$BLOCK_BUTTON" -eq 1 ]; then # If Clicked on this Block,
-  /usr/local/bin/Scripts/Bluetooth/blueoff.sh # Execute blueoff.sh Script.
-fi
--- scripts/Tools/share.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-filename="$1"
-upload_url=$(curl -F "file=@$filename" https://0x0.st)
-echo -n "$upload_url" | wl-copy
-notify-send "URL Copied" "URL: $upload_url"
